2db7131029d697d828c1ef49abcd28de7e8d06af
Franco Sanchez Alejandro <francosanche@gmail.com>
2023-02-13 21:53:28 -0300

    some changes of routes

---
+++ src/routes/users.router.js
@@ -0,0 +1,29 @@
+const {Router} = require('express')
+
+const router = Router()
+
+router.get("/", (req, res) => {
+    // to do
+})
+
+router.get("/:id", (req, res) => {
+    // to do
+})
+
+router.post("/", (req, res) => {
+    // to do
+})
+
+router.put("/:id", (req, res) => {
+    // to do
+})
+
+router.delete("/:id", (req, res) => {
+    // to do
+})
+
+module.exports = router
+
+
+
+
